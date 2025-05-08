077954a36161edc3567a19eaa524448a27437960
Furqan Shah <furqanfff6@gmail.com>
2025-05-09 03:58:22 +0500

    Deploy Flutter Web to GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -45,7 +45,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b3090c1761b3fbf45725486e77b14528
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "31069aff8b29b44851a3a938454ca5da",
+"flutter_bootstrap.js": "8e8e4c7c1f3e3e8cebd3418d0b4ee60b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
