4038e809043b502b2e64c1bebb8ae8922ac9c7d7
Furqan Shah <furqanfff6@gmail.com>
2025-05-09 04:24:01 +0500

    Initial deploy

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "197906477"
+    serviceWorkerVersion: "1556669864"
   }
 });
